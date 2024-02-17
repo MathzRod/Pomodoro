435ee2e71b65109617ec8ee412f0b0553c3c3f7d
Matheus Rodrigues de Camargo <mathzrodrsocial@gmail.com>
2024-02-17 18:08:39 -0300

    Insert style into buttons

--- script.js
+++ script.js
@@ -4,23 +4,35 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo')
 
+const botoes = document.querySelectorAll('.app__card-button');
+
 const banner = document.querySelector('.app__image');
 
 const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+
+    // adicionará uma classe para inserir o estilo ao botao no qual cliquei
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto){
+
+    //assim que eu clicar em um botao ele removerá todos e inserirá apenas o que está ativo acima ^
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
